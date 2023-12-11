bbd3c38925c4db825cf08aca59b8be49905c7fb1
Tanushree2001 <tanushree.gupta@innoraft.com>
2023-12-11 15:29:38 +0530

    optimizing

--- js/script.js
+++ js/script.js
@@ -1,107 +1,105 @@
-$(document).ready(function () {
-  $(function () {
-    $(".slider").slick({
-      dots: true,
-      infinite: true,
-      speed: 300,
-      autoplay: false,
-      autoplaySpeed: 2000,
-      slidesToShow: 1,
-      arrows: false,
-      adaptiveHeight: true,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            infinite: true,
-            dots: true,
-          },
+$(function () {
+  $(".slider").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    slidesToShow: 1,
+    arrows: false,
+    adaptiveHeight: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
         },
-        {
-          breakpoint: 992,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-          },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
         },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-          },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
         },
-      ],
-    });
+      },
+    ],
+  });
 
-    $(".responsive").slick({
-      dots: true,
-      infinite: true,
-      speed: 300,
-      autoplay: false,
-      autoplaySpeed: 1000,
-      slidesToShow: 3,
-      adaptiveHeight: true,
-      arrows: false,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            infinite: true,
-            dots: true,
-          },
+  $(".responsive").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    autoplay: false,
+    autoplaySpeed: 1000,
+    slidesToShow: 3,
+    adaptiveHeight: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
         },
-        {
-          breakpoint: 992,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-          },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
         },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-          },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
         },
-      ],
-    });
+      },
+    ],
+  });
 
-    // Function on the position of the second section
-    function secondSectionPosition() {
-      return $(".about__part").offset().top;
+  // Function on the position of the second section
+  function secondSectionPosition() {
+    return $(".about__part").offset().top;
+  }
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > secondSectionPosition()) {
+      $(".contact-button").fadeIn();
+    } else {
+      $(".contact-button").fadeOut();
+      $(".contact-form").fadeOut();
     }
+  });
 
-    $(window).scroll(function () {
-      if ($(this).scrollTop() > secondSectionPosition()) {
-        $(".contact-button").fadeIn();
-      } else {
-        $(".contact-button").fadeOut();
-        $(".contact-form").fadeOut();
-      }
-    });
+  $(".contact__Btn").click(function () {
+    $(".contact-form").fadeIn();
+    $(".contact__Btn").fadeOut();
+  });
 
-    $(".contact__Btn").click(function () {
-      $(".contact-form").fadeIn();
-      $(".contact__Btn").fadeOut();
-    });
+  $(".cross").click(function () {
+    $(".contact-form").fadeOut();
+    $(".contact__Btn").fadeIn();
+  });
 
-    $(".cross").click(function () {
-      $(".contact-form").fadeOut();
-      $(".contact__Btn").fadeIn();
-    });
-
-    $("#contactForm").submit(function (e) {
-      e.preventDefault();
-      // Handled form submission here
-      // Reset the form after submission
-      $("#contactForm")[0].reset();
-      $(".contact-form").fadeOut();
-    });
+  $("#contactForm").submit(function (e) {
+    e.preventDefault();
+    // Handled form submission here
+    // Reset the form after submission
+    $("#contactForm")[0].reset();
+    $(".contact-form").fadeOut();
   });
 });
